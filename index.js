99c837918e09a78639e9779c1c2b5c8c92096a7e
Sam De Maeyer <sam_demaeyer@hotmail.com>
2019-12-31 14:43:58 +0100

    Restart animation after each 10 seconds

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let timing = 1000;
+let timing = 0;
 
-document.querySelectorAll('.piece').forEach(p => {
-  setTimeout(() => {
-    p.style.display = 'block';
-  }, timing += 750);
-});
+const startHangman = () => {
+  document.querySelectorAll('.piece').forEach(p => setTimeout(() => p.style.display = 'block', timing += 750));
+}
+
+setInterval(() => {
+  timing = 0;
+  document.querySelectorAll('.piece').forEach(p => p.style.display = 'none');
+  startHangman();
+}, 10000);
+
+startHangman();
