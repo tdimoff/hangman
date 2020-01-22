4d7bfd036887ef3482677263473eb19f9b35a834
Kira Slobodina <slobodina.kira@gmail.com>
2020-01-22 21:01:32 +0000

    Add game logic

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-let timing = 0;
+const wordToGuess = 'codebar'
 
-const startHangman = () => {
-  document.querySelectorAll('.piece').forEach(p => setTimeout(() => p.style.display = 'block', timing += 750));
-}
-
-setInterval(() => {
-  timing = 0;
-  document.querySelectorAll('.piece').forEach(p => p.style.display = 'none');
-  startHangman();
-}, 10000);
-
-startHangman();
+document.querySelector('.letter-input').addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    const valueIndex = wordToGuess.split('').indexOf(event.target.value);
+    if (valueIndex > -1) {
+      document.querySelectorAll('.letter')[valueIndex].innerText = event.target.value;
+    }
+  }
+})
